65f7e9269de2ee1aebb2b60192a0cf02c58180cc
555-Freyja <58935586+555-Freyja@users.noreply.github.com>
2021-09-12 20:28:53 +0800

    Add files via upload

---
+++ week4/week4/柠檬水找零.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        int five = 0, ten = 0, twenty = 0;
+        for (int bill : bills) {
+            // 情况一
+            if (bill == 5) five++;
+            // 情况二
+            if (bill == 10) {
+                if (five <= 0) return false;
+                ten++;
+                five--;
+            }
+            // 情况三
+            if (bill == 20) {
+                if (five > 0 && ten > 0) {
+                    five--;
+                    ten--;
+                } else if (five >= 3) {
+                    five -= 3;
+                } else return false;
+            }
+        }
+        return true;
+    }
+};
